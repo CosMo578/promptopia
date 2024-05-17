4eb8d1cbd6f5c883fbc1662f21c09691cd8fb9f9
CosMo578 <raphaelakpor00@gmail.com>
2024-05-17 02:52:35 +0100

    First Commit Lost Some Folders - api - models etc

---
+++ src/components/Feed.jsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import PromptCard from './PromptCard';
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState()
+  return (
+    <section className="feed">
+      <form className="w-full relative flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchText}
+          required
+          className='search_input peer'
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Feed;
